d3e4872d1349133e287270662a84a9476109f60e
91636 <91636@MSI>
2025-05-23 07:38:07 +0530

    first commit

--- src/com/tns/day5/secondpackage/Executor.java
+++ src/com/tns/day5/secondpackage/Executor.java
@@ -1,14 +1,14 @@
 package com.tns.day5.secondpackage;
 
-import com.tns.day5.base_executor;
+//import com.tns.day5.base_executor;
 
 public class Executor {
 
 	public static void main(String[] args) {
 		
-		Base b1=new Base();
+		//Base b1=new Base();
 		//b1.varDefault=40;
-		b1.varPublic=70;
+		//b1.varPublic=70;
 	//	b1.varProtected=90;
 		//b1.varPrivate=80;
 
